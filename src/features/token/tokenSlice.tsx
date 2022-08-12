f94892cc5cf2f7c4d2129ac423861fc6cd2ec037
karan-ksrk <karanksrk2000@outlook.com>
2022-08-12 16:47:42 +0530

    added modal and other ui fixes

--- src/features/token/tokenSlice.tsx
+++ src/features/token/tokenSlice.tsx
@@ -1,7 +1,9 @@
+import { useMemo } from 'react';
 import { createSlice, createAsyncThunk, AnyAction } from '@reduxjs/toolkit';
-import { Connection, PublicKey } from '@solana/web3.js';
-import { Program, BN, AnchorProvider } from '@project-serum/anchor';
+import { Connection, Keypair, PublicKey } from '@solana/web3.js';
+import { Program, BN, AnchorProvider, Provider } from '@project-serum/anchor';
 import { AnchorWallet } from '@solana/wallet-adapter-react';
+import * as anchor from '@project-serum/anchor';
 
 import {
     TOKEN_PROGRAM_ID,
@@ -12,6 +14,8 @@ import {
     AccountLayout,
 } from "@solana/spl-token";
 
+import idl from '../../idl.json';
+
 const network = "https://api.devnet.solana.com";
 
 const initialState = {
@@ -20,6 +24,8 @@ const initialState = {
     isRequestSending: false,
     mintInputs: [],
     provider: typeof AnchorProvider,
+    currentMintKey: null,
+    isTokenRequestSending: false,
 }
 
 export const getProvider = createAsyncThunk('token/getProvider', async (wallet: AnchorWallet) => {
@@ -32,7 +38,7 @@ export const getProvider = createAsyncThunk('token/getProvider', async (wallet: 
     return provider
 })
 
-export const getTokens = createAsyncThunk('token/getTokenAccount', async (wallet: AnchorWallet, { dispatch }) => {
+export const getTokens = createAsyncThunk('token/getTokenAccount', async (wallet: AnchorWallet) => {
 
     const connection = new Connection(network, 'processed')
 
@@ -42,7 +48,6 @@ export const getTokens = createAsyncThunk('token/getTokenAccount', async (wallet
             programId: TOKEN_PROGRAM_ID,
         }
     )
-    console.log(accounts)
     return accounts
 
 
@@ -63,10 +68,77 @@ export const getAtaAccount = createAsyncThunk('token/getATAAccount', async (data
     return [...ataAccounts];
 })
 
+export const mintToken = createAsyncThunk('token/mintToken', async (data: any, { dispatch }) => {
+    const a = JSON.stringify(idl)
+    const b = JSON.parse(a)
+    const provider: Provider = data.provider;
+    const tokenAddress: PublicKey = data.tokenAddress;
+    const mintAmount: number = data.mintAmount;
+    const ataAddress: PublicKey = data.ataAddress;
+    const wallet: AnchorWallet = data.wallet;
+    const program = new Program(b, idl.metadata.address, provider);
+
+    try {
+        await program.methods.mintToken(new BN(mintAmount)).accounts({
+            mint: tokenAddress,
+            tokenProgram: TOKEN_PROGRAM_ID,
+            tokenAccount: ataAddress,
+            signer: wallet.publicKey
+        }).rpc();
+
+        dispatch(getTokens(wallet));
+    }
+    catch (err) {
+        console.log(err)
+    }
+})
+
+export const createToken = createAsyncThunk('token/createToken', async (data: any, { dispatch }) => {
+
+    const a = JSON.stringify(idl);
+    const b = JSON.parse(a);
+    const provider = data.provider;
+    const wallet: AnchorWallet = data.wallet;
+    const mintKey = anchor.web3.Keypair.generate();
+
+    const program = new Program(b, idl.metadata.address, provider);
+    const lamports: number = await program.provider.connection.getMinimumBalanceForRentExemption(MINT_SIZE);
+    const associatedTokenAccount = await getAssociatedTokenAddress(
+        mintKey.publicKey,
+        wallet.publicKey,
+    )
+
+    const token_txn = new anchor.web3.Transaction().add(
+        anchor.web3.SystemProgram.createAccount({
+            fromPubkey: wallet.publicKey,
+            newAccountPubkey: mintKey.publicKey,
+            space: MINT_SIZE,
+            programId: TOKEN_PROGRAM_ID,
+            lamports,
+        }),
+
+        createInitializeMintInstruction(
+            mintKey.publicKey, 0, wallet.publicKey, wallet.publicKey
+        ),
+        createAssociatedTokenAccountInstruction(wallet.publicKey, associatedTokenAccount, wallet.publicKey, mintKey.publicKey)
+    );
+    // try {
+    const res = await provider?.sendAndConfirm(token_txn, [mintKey]);
+    dispatch(getTokens(wallet));
+    if (res) {
+        return mintKey;
+    }
+    // } catch (e) {
+    //     console.log(e);
+    // }
+})
+
+
 const tokenSlice = createSlice({
     name: 'token',
     initialState,
     reducers: {
+
     },
     extraReducers: builder => {
         builder.addCase(getProvider.fulfilled, (state, action: AnyAction) => {
@@ -87,9 +159,21 @@ const tokenSlice = createSlice({
         builder.addCase(getAtaAccount.fulfilled, (state, action: AnyAction) => {
             state.ataAccounts = action.payload;
         })
+        builder.addCase(createToken.pending, (state) => {
+            state.isTokenRequestSending = true;
+        })
+        builder.addCase(createToken.fulfilled, (state, action: AnyAction) => {
+            state.isTokenRequestSending = false;
+            state.currentMintKey = action.payload;
+        })
+        builder.addCase(createToken.rejected, (state, action: AnyAction) => {
+            console.log(action.payload);
+            state.isTokenRequestSending = false;
+        })
     },
 
 })
 
 
 export default tokenSlice.reducer;
+export const { } = tokenSlice.actions;
